fda05ca0f2aa90d1f705da21d5d3b6dc5a759969
Mina Aziz <madel0452@gmail.com>
2025-02-18 13:28:07 -0800

    Updated portfolio content

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Mina-Portfolio/',
-  plugins: [react()],
+  base: '/Mina-Portfolio/', // Repository name for GitHub Pages
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html', // Source file to copy
+          dest: '', // Destination for the copied file (root of dist folder)
+          rename: '404.html', // Rename index.html to 404.html
+        },
+      ],
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
@@ -12,4 +24,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
